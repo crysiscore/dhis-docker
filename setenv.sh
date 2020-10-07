8fff640a34c8e98f9ae88a651b24766ab5472bf6
Agnaldo <agnaldosamuel3@mail.com>
2020-10-07 22:16:33 +0200

    first commit

---
+++ setenv.sh
@@ -0,0 +1,3 @@
+
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -Xms4000m -Xmx7500m -XX:PermSize=2056m -XX:MaxPermSize=2056m -XX:-UseGCOverheadLimit -XX:+UseParallelGC  -server"
+export DHIS2_HOME='/home/dhis/config'
